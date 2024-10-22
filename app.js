4ba4fb0ef7f6256e29f0d3ce2004ceed2ab3da60
thisnotulala <161106097+keyshi8@users.noreply.github.com>
2024-10-22 10:59:23 +0700

    commit api

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const expressLayouts = require("express-ejs-layouts");
+const connectDB = require("./app_api/models/db")
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var prodiRouter = require('./app_server/routes/prodi');
+const fakultasRouter = require('./app_api/routes/fakultas');
 
 var app = express();
 
@@ -25,6 +27,9 @@ app.use(expressLayouts);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/prodi', prodiRouter);
+app.use('/api/fakultas', fakultasRouter);
+
+connectDB();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app_api/models/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            //"mongodb+srv://ricky:juarasatu1@cluster0.du3lb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb://localhost:27017/mdpb"
+        );
+
+        console.log("MongoDB Connected...");
+    } catch (error) {
+        console.error("MongoDB connection error: ", error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB
